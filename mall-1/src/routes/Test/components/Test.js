240f48ac31720161d3077ac8aef0b07c959ab0b9
yishuangxi <2546618112@qq.com>
2016-10-12 16:38:53 +0800

    add mall-1

---
+++ mall-1/src/routes/Test/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Test = (props) => (
+  <div style={{ margin: '0 auto' }} >
+    This is test page
+  </div>
+)
+
+Test.propTypes = {
+}
+
+export default Test
